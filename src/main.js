5435f0470a10cca2b1eb0f287831cc496e026639
Yuliia Blahovistna <yublagov@gmail.com>
2025-10-28 15:41:42 +0100

    add forEach

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import './style.css'
-const containerEl = document.getElementById(‘app’)
+
+const containerEl = document.getElementById('app')
+
 const createImage = (src) => {
-  const imageEl = document.createElement(‘img’)
-  imageEl.src = src;
+  const imageEl = document.createElement('img')
+  imageEl.src = src
   containerEl.appendChild(imageEl)
 }
 
 const init = () => {
-  fetch('https://image-feed-api.vercel.app/api/images/e8cd3ffd-794c-4ec6-b375-7788dbb14275')
+  fetch('https://image-feed-api.vercel.app/api/images?page=1')
     .then(resp => resp.json())
-    .then(json => createImage(json.image_url))
+    .then(json => json.data.forEach(img => createImage(img.image_url)))
 }
 
-init();
+init()
+
